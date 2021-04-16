50b1f75f2a593ef97059bda93a8567bfdb67a622
esfreitas <44111600+esfreitas@users.noreply.github.com>
2021-04-16 06:35:29 -0300

    fazendo desacoplamento dos components, ainda está apresentando erro

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
-import './App.css';
-import { useState } from 'react';
-import {FaRegCheckSquare, FaRegSquare} from "react-icons/fa";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import TaskLists from "./components/TaskLists";
+import Tasks from "./components/Tasks";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
 
 
 
 
 function App() {
 
-  const [list, setList] = useState([]);
-
-  function adicionar(e) {
-    e.preventDefault();
-    console.log(e.target.task.value);
-    const task = {
-      id:new Date(),
-      name: e.target.task.value,
-      status: "pendente",
-    };
-    setList([...list, task]);
-  }
-
-  function toggle(item) {
-    const statusToUpdate = item.status ==="pendente" ? "feito" : "pendente";
-    const newList = list.map((t) => {
-      if (t.id === item.id) t.status = statusToUpdate;
-      return t;
-    });
-    setList(newList);
-  }
-
   return (
-    <div className="App">
-     <form onSubmit={adicionar}>
-        <label>
-           <input type="text" name="task" />
-        </label>
-        <input type="submit" value="Adicionar"/>
-     </form>
-     <ul>
-       {list.map((item, index)=>{
-         return (
-          <li style={item.status === "feito" ? {textDecoration:"line-through"} : {}} key={index}>
-            <span>{item.name}</span>
-            <button onClick={()=> toggle(item)}>
-                {item.status ==="feito" ?
-                  <FaRegCheckSquare /> : 
-                  <FaRegSquare />}
-              </button>
-          </li>
-       );
-       })}
-     </ul>
-    </div>
+    <Layout.Content style={{padding: 20}}>
+      <Router>
+        <Link to="/">Lista de Tarefas</Link> | <Link to="/list/1">Tarefas</Link> 
+        <Switch>
+          <Route exact path="/">
+              <TaskLists />
+          </Route>
+          <Route exact path="/list/:id">
+              <Tasks />
+          </Route>
+        </Switch>
+      </Router>
+    </Layout.Content>
   );
 }
 
---
+++ src/components/Tasks.js
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { FaRegCheckSquare, FaRegSquare } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+
+
+
+const Tasks = () =>{
+
+  const params = useParams();
+  const [list, setList] = useState([]);
+  const [listFilter, setListFilter] = useState([]);
+  const [isOnlyPending, setIsOnlyPending] = useState(false);
+  const [isEditing, setIsEditing] = useState("");
+
+  useEffect(()=>{
+      console.log(params);
+  },[params])
+
+  function adicionar(e) {
+    e.preventDefault();
+    console.log(e.target.task.value);
+    const task = {
+      id:new Date(),
+      name: e.target.task.value,
+      status: "pendente",
+    };
+    setList([...list, task]);
+    setListFilter([...list, task]);
+    document.getElementById("task").value="";
+  }
+
+  function toggle(item) {
+    const statusToUpdate = item.status ==="pendente" ? "feito" : "pendente";
+    const newList = list.map((t) => {
+      if (t.id === item.id) t.status = statusToUpdate;
+      return t;
+    });
+    setList(newList);
+  }   
+
+  function filter(){
+    
+    const listToFilter = listFilter.filter(item => {
+      return isOnlyPending ? item.status === "pendente" : true;
+    })
+    setList(listToFilter);
+    setIsOnlyPending(!isOnlyPending);
+  }
+
+  function save(newName, item){
+      const newList = list.map((t)=>{
+          if(t.id === item.id) t.name = newName;
+          return t;
+      });
+      setList(newList);
+      setIsEditing("");
+  }
+
+  function onKeyDown(e, item){
+      if(e.charCode === 13 || e.keyCode === 13) save(e.target.value, item);
+  }
+
+  function onBlur(e, item){
+      save(e.target.value, item);
+  }
+
+ 
+
+  return (
+    <div className="App">
+     <form onSubmit={adicionar}>
+        <label>
+           <input type="text" name="task" />
+        </label>
+        <input type="submit" value="Adicionar"/>
+     </form>
+     <ul>
+       {list.map((item, index)=>{
+         return (
+          <li style={item.status === "feito" ? {textDecoration:"line-through"} : {}} key={index}>
+            <span>
+                {isEditing === item.id ? (
+                    <input defaultValue={item.name} onBlur={(e)=>onBlur(e, item)} onKeyDown={(e)=>onKeyDown(e, item)} />
+                ) : (
+                    <b onClick={()=>toggle(item)}>{item.name}</b>
+                )}
+            </span>
+            <button onClick={()=> toggle(item)}>
+                {item.status ==="feito" ?
+                  <FaRegCheckSquare /> : 
+                  <FaRegSquare />}
+              </button>
+          </li>
+       );
+       })}
+     </ul>
+    </div>
+  );
+};
+
+export default Tasks;
